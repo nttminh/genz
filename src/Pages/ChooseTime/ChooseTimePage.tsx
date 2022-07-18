e2c7633ecb9c40a9c1fc35e92faff7098f9861a3
Minh Nguyen <ng.minh@hotmail.com>
2022-07-18 15:57:05 +0700

    fix: 🐛 handle when movieShowTime is null

--- src/Pages/ChooseTime/ChooseTimePage.tsx
+++ src/Pages/ChooseTime/ChooseTimePage.tsx
@@ -2,9 +2,10 @@ import { Button, Group, Tabs } from '@mantine/core';
 import BackButton from 'Components/BackButton';
 import { AppDispatch, RootState } from 'configStore';
 import { formatDate } from 'Helpers/formatDate';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getMovieShowTimes } from 'Slices/cart';
 
 type Props = {};
 
@@ -20,12 +21,17 @@ const ChooseTimePage = (props: Props) => {
 		console.log('tabKey', tabKey);
 	};
 
+	useEffect(() => {
+		if (!movieShowTimes) {
+			dispatch(getMovieShowTimes(id!));
+		}
+	}, []);
+
 	return (
 		<div className="p-4">
 			<BackButton />
 			<h1>{movieShowTimes?.tenPhim}</h1>
 			<p>Ngày khởi chiếu: {formatDate(movieShowTimes?.ngayKhoiChieu)}</p>
-			<p>Time will be updated later</p>
 			<Tabs active={activeTab} onTabChange={onChange}>
 				<Tabs.Tab label="22/7">
 					<Group>
